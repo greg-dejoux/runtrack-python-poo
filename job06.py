65b4b4f327faa67f80ff7bad653d91a67ec449dd
greg-dejoux <127529754+greg-dejoux@users.noreply.github.com>
2023-03-28 11:01:58 +0200

    Delete job06.py
    reupload dans dossier jour01

--- job06.py
+++
@@ -1,7 +0,0 @@
-class Rectangle:
-    def __init__(self,largeur,longueur):
-        self.__largeur = largeur
-        self.__longueur = longueur
-        print("Longueur =", self.__longueur, "\nLargeur =", self.__largeur)
-
-animal1 = Rectangle(10,5)
